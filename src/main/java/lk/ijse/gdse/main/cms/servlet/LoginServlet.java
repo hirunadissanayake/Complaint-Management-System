66722cb2e158f091711711ddb5c47e05ec267a29
hirunadissanyake <hirunasankalpa@gmail.com>
2025-06-16 17:53:04 +0530

    first commit

---
+++ src/main/java/lk/ijse/gdse/main/cms/servlet/LoginServlet.java
@@ -0,0 +1,22 @@
+package lk.ijse.gdse.main.cms.servlet;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+@WebServlet("/Login")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+        String role = req.getParameter("role");
+
+        System.out.println("name: " + username + " password: " + password + " role: " + role);
+
+    }
+}
